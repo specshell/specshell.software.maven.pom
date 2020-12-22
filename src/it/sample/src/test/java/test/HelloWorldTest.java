12f8438efc17a62b7875ebedec4c43144cde2418
Joseph Petersen <jpe@specshell.com>
2020-12-22 13:59:23 +0100

    add junit 5 test dependency (#17)

--- src/it/sample/src/test/java/test/HelloWorldTest.java
+++ src/it/sample/src/test/java/test/HelloWorldTest.java
@@ -1,14 +1,14 @@
 package test;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class HelloWorldTest {
 
     @Test
     public void smokes() throws Exception {
-      HelloWorld helloWorld = new HelloWorld("Hello World");
-
+        HelloWorld helloWorld = new HelloWorld("Hello World");
         assertEquals(helloWorld.getStr(), "Hello World");
     }
 
